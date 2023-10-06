590ccc3024deca48424da793ce6a26a9f6f6a093
neftalito <47830827+neftalito@users.noreply.github.com>
2023-10-06 12:41:11 -0300

    Update ObtenerNotas.js

--- ObtenerNotas.js
+++ ObtenerNotas.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         Mostrar Notas
 // @namespace    http://tampermonkey.net/
+// @license MIT
 // @version      0.1
 // @description  Script para mostrar las notas actuales de todos los usuarios en el scoreboard del CTFd.
 // @author       Neftalí Toledo
